5f193a6efc74aaacf298ede5f4891863e93f0256
zyz930713 <13952846903@163.com>
2019-09-18 14:38:50 +0800

    first

---
+++ src/views/home/index.vue
@@ -0,0 +1,39 @@
+<!-- home -->
+<template>
+  <div>
+    <h1>home</h1>
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+// import { api } from '@/config'
+import {
+  Button
+} from 'vant'
+export default {
+  components: {
+    'van-button': Button
+  },
+
+  data () {
+    return {}
+  },
+
+  computed: {},
+
+  mounted () {
+    // console.log(process.env)
+    console.log('hello world')
+  },
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+h1 {
+  background: red;
+  width: 375px;
+}
+</style>
